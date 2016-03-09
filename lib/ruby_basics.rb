1a6bc9be51603a6c47b60f2545b2fd18c425ef99
datboiez <hill.eric@gmail.com>
2016-03-08 21:26:25 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+ 12 / 2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(value)
+  value = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(name1, name2)
+  name1 = "User" 
+  name2 = "Howard"
+  puts "Hello #{name1}, my name is #{name2} "
 end
 
 def return_a_value
-
+return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
 
+def pizza_party(type = "cheese")
+puts "#{type}"
+type
 end
